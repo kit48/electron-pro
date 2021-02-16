0d460d58d6615230f357c3d874d6f907a4423ca1
Yuns <yuns.xie@qq.com>
2021-02-16 21:35:57 +0800

    feat: dynamicImport loading

--- src/renderer/config/config.ts
+++ src/renderer/config/config.ts
@@ -1,5 +1,5 @@
 // https://umijs.org/config/
-import { defineConfig } from 'umi';
+import { defineConfig, IConfig } from 'umi';
 
 import routes from './routes';
 
@@ -9,9 +9,16 @@ export default defineConfig({
     type: 'hash',
   },
   title: 'Electron Pro',
-  dynamicImport: {},
+  // 开发环境通过 http 服务器分发资源，开启 dynamicImport
+  // 生产环境直接读取内部文件，几乎不存在延迟，无需开启
+  dynamicImport:
+    process.env.NODE_ENV === 'development'
+      ? {
+          loading: '@/components/PageLoading',
+        }
+      : false,
   // umi routes: https://umijs.org/docs/routing
   routes,
   publicPath: './',
   outputPath: `../../dist/renderer`,
-});
+} as IConfig);
